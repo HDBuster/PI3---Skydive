484319a254d18955839777e201a4d1efce26383e
Breno de Souza Vieira <brenosouzavieira15@gmail.com>
2024-04-03 21:34:54 -0300

    Add parachute drag.

---
+++ Assets/Scripts/Player/Parachute.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Parachute : MonoBehaviour
+{
+    [SerializeField] InputActionAsset input;
+    Rigidbody rb;
+
+    [SerializeField] [Range(1, 5)] float parachuteDrag;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (input.FindActionMap("Player").FindAction("parachute").ReadValue<float>() == 1)
+        {
+            rb.drag = parachuteDrag;
+        }
+    }
+}
