a7359f02738bb92b77105c698f142899f9b9ea02
Breno de Souza Vieira <brenosouzavieira15@gmail.com>
2024-04-10 15:56:54 -0300

    Change camera position code. Add camera shaking

--- Assets/Scripts/Player/Camera/CameraPosition.cs
+++ Assets/Scripts/Player/Camera/CameraPosition.cs
@@ -1,25 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UIElements;
 
 public class CameraPosition : MonoBehaviour
 {
     [SerializeField] GameObject player;
 
-    // Start is called before the first frame update
+    [SerializeField] float shakeLimit;
+    [SerializeField] float shakeForce;
+    [SerializeField] float shakeWait;
+
+    [SerializeField] float shake;
+
+    Rigidbody prb;
+
     void Start()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        prb = player.GetComponent<Rigidbody>();
     }
 
     private void FixedUpdate()
     {
-        this.transform.position = player.transform.position + new Vector3 (0, 4, 1);
+        shake = Mathf.Clamp(Mathf.Abs(prb.velocity.y) * shakeForce, shakeLimit * -1, shakeLimit);
+        this.transform.position = player.transform.localPosition + new Vector3(0, Random.Range(shake * -1, shake) + 4, 1);
     }
 }
