35303ef052be84993302622f204e4ea54be358a1
Breno de Souza Vieira <brenosouzavieira15@gmail.com>
2024-04-29 21:41:41 -0300

    Only next circle below player will appear.

---
+++ Assets/Scripts/Gameplay/Circles/CirclesVisibility.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CirclesVisibility : MonoBehaviour
+{
+    GameObject player;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        foreach (Transform child in this.transform)
+        {
+            if ((player.transform.position.y < child.position.y + 100 && player.transform.position.y > child.position.y - 100))
+            {
+                child.gameObject.GetComponent<MeshRenderer>().enabled = true;
+            }
+            else
+            {
+                child.gameObject.GetComponent<MeshRenderer>().enabled = false;
+            }
+        }
+    }
+}
