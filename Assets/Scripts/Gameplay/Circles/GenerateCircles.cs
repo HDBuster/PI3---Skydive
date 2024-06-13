edf4e7f17c819430d404fa6eb153df9cc75aec01
Breno de Souza Vieira <brenosouzavieira15@gmail.com>
2024-06-13 10:26:27 -0300

    Randomize spheres color

--- Assets/Scripts/Gameplay/Circles/GenerateCircles.cs
+++ Assets/Scripts/Gameplay/Circles/GenerateCircles.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class GenerateCircles : MonoBehaviour
 {
     [SerializeField] GameObject circle;
+    [Header("Circles")]
+    [SerializeField] GameObject redCircle;
+    [SerializeField] GameObject greenCircle;
+    [SerializeField] GameObject blueCircle;
+    [SerializeField] GameObject yellowCircle;
 
+    [Header("References")]
     [SerializeField] GameObject player;
     [SerializeField] GameObject target;
 
@@ -21,9 +27,31 @@ public class GenerateCircles : MonoBehaviour
             Instantiate(circle, new Vector3(0, -1 , 0), Quaternion.identity, this.transform);
         }*/
 
-        for (int i = 1; i < ((playerHeight - 200) * 0.005f); i++) // Instantiate circles. Quantity depends on player initial height.
+        /*for (int i = 1; i < ((playerHeight - 200) * 0.005f); i++) // Instantiate circles. Quantity depends on player initial height.
         {
             Instantiate(circle, new Vector3(0, -1, 0), Quaternion.identity, this.transform);
+        }*/
+
+        for (int i = 1; i < ((playerHeight - 200) * 0.005f); i++) // Instantiate circles. Quantity depends on player initial height.
+        {
+            var random = Random.Range(0, 4);
+            switch (random)
+            {
+                case 0:
+                    Instantiate(redCircle, new Vector3(0, -1, 0), Quaternion.identity, this.transform);
+                    break;
+                case 1:
+                    Instantiate(greenCircle, new Vector3(0, -1, 0), Quaternion.identity, this.transform);
+                    break;
+                case 2:
+                    Instantiate(blueCircle, new Vector3(0, -1, 0), Quaternion.identity, this.transform);
+                    break;
+                case 3:
+                    Instantiate(yellowCircle, new Vector3(0, -1, 0), Quaternion.identity, this.transform);
+                    break;
+
+            }
+            //Instantiate(circle, new Vector3(0, -1, 0), Quaternion.identity, this.transform);
         }
 
         foreach (Transform child in this.transform) //Change position of each circle.
